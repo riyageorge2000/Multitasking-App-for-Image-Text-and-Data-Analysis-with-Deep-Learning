6e3a1ffc2a6e50d86c33a8a1337a373102defae3
Riya George <127472265+riyageorge2000@users.noreply.github.com>
2023-11-18 10:20:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,29 +12,31 @@ import pickle
 import requests
 from io import BytesIO
 import tempfile
+import h5py
 
-# Function to download the model from Google Drive URL
-def download_model_from_drive(drive_url):
+# Function to download the model from Google Drive URL and save as an HDF5 file
+def download_model_from_drive(drive_url, file_path):
     # Extract the file ID from the URL
     file_id = drive_url.split('/')[-2]
+    
     # Create the download URL
     download_url = f"https://drive.google.com/uc?id={file_id}"
+    
     # Fetch the content from the download URL
     response = requests.get(download_url)
-    return response.content
+    
+    # Save the content directly as an HDF5 file
+    with h5py.File(file_path, 'w') as file:
+        file.create_dataset('model', data=response.content)
 
+    return file_path
+# Define the path to save the model
+model_path = 'model.h5'
 # Load the tumor classification model
 drive_url = "https://drive.google.com/file/d/1QY-MI1Sc7MVYiQuFHoCiaq9PD4VqS2TD/view?usp=sharing"
-cnn_model_content = download_model_from_drive(drive_url)
-# Save the downloaded content to a temporary file
-with tempfile.NamedTemporaryFile(delete=False) as temp_file:
-    temp_file.write(cnn_model_content)
-    model_path = temp_file.name
-# Load the model from the temporary file
+download_model_from_drive(drive_url, model_path)
+# Load the model from the HDF5 file
 cnn_model = tf.keras.models.load_model(model_path)
-
-
-
 
 
 # Load your tumor classification model
